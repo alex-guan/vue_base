4bcf5d95a088afb186fe666383d39681e367687b
guanxiangjie <guanxiangjie2017@163.com>
2019-04-21 22:03:33 +0800

    基本配置完成

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Button @click="testHttp">iview</Button>
+    <DatePicker></DatePicker>
+    {{res}}
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      res : ""
+    }
+  },
+  methods: {
+    async testHttp(){
+         this.res = await this.$http.get("").then(data=>data.data)
+        }
+  },
+}
+</script>
+
+<style lang="less">
+
+</style>
---
+++ src/axios.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+//创建axios实例
+var instance = axios.create({
+    baseURL: '/api'
+});
+
+//拦截器
+instance.interceptors.response.use(
+    function(response){
+        return response;
+    },function(error){
+         if(error.response.status==404){
+            return {
+                data : "Hello Axios"
+            }
+        } 
+    })
+export default instance;
